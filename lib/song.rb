23d5d1f939014ef3f43bbcad971bbf36174f4495
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-17 00:43:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,22 +1,22 @@
 require 'pry'
 
-class Song
-  attr_accessor :name
-  attr_reader :artist
+# class Song
+#   attr_accessor :name
+#   attr_reader :artist
 
-extend Memorable::ClassMethods
-include Memorable::InstanceMethods
-extend Findable
-include Paramable
+# extend Memorable::ClassMethods
+# include Memorable::InstanceMethods
+# extend Findable
+# include Paramable
 
-  @@songs = []
+#   @@songs = []
 
-  def self.all
-    @@songs
-  end
+#   def self.all
+#     @@songs
+#   end
 
-  def artist=(artist)
-    @artist = artist
-  end
+#   def artist=(artist)
+#     @artist = artist
+#   end
 
-end
+# end
